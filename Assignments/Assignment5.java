7a3af323f0880c607428f101ff6f3a43ec2e8ed7
Mahesh7036 <96856448+Mahesh7036@users.noreply.github.com>
2022-01-02 21:47:14 +0530

    Add files via upload

---
+++ Assignments/Assignment5.java
@@ -0,0 +1,21 @@
+package Assignments;
+
+public class Assignment5 {
+
+        public static void main(String[] args) {
+            
+            int [] arr = {7,4,3,6,8,5,1,9};
+            System.out.println(largestInArray(arr));
+            
+        }
+    
+        static int largestInArray(int [] a) {
+                int max = a[0];
+                for(int i=1;i<a.length;i++) {
+                    if(a[i] > max) {
+                        max = a[i];
+                    }
+                }
+                return max;
+        }
+  }
