03ad4ea7bdc0acffe04cdb3ecf0d86c2155d6583
Mahesh7036 <96856448+Mahesh7036@users.noreply.github.com>
2022-01-10 11:00:40 +0530

    Create Victory.java

---
+++ Additional Practice 1/Victory.java
@@ -0,0 +1,18 @@
+
+public class Victory {
+
+	public static void main(String[] args) {
+		
+		Pattern("/");
+		System.out.print("\n|| Victory is mine! || \n");
+		Pattern("\\");
+		
+
+	}
+	static void Pattern(String m) {
+		for(int i=0;i<=21;i++) {
+			System.out.print(m);
+		}
+	}
+
+}
